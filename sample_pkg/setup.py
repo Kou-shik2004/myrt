d68f1e250511c2f4387291009ce667aacdd08b69
Kou-shik2004 <koushik20040804@gmail.com>
2024-06-17 03:08:25 +0530

    added sample pub and sub pkg for testing

---
+++ sample_pkg/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+package_name = 'sample_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='pi',
+    maintainer_email='pi@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': ["pub_node=sample_pkg.simple_pub:main","sub_node=sample_pkg.simple_sub:main"
+        ],
+    },
+)
