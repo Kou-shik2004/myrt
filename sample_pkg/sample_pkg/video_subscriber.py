2f6a0924ae3b2cfc82a64979ef03fe4e91c8c6e3
Kou-shik2004 <koushik20040804@gmail.com>
2024-06-20 10:56:54 +0530

    added video subsriber.py

---
+++ sample_pkg/sample_pkg/video_subscriber.py
@@ -0,0 +1,37 @@
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+from std_msgs.msg import String
+import cv2
+from cv_bridge import CvBridge
+
+
+class Camera_sub(Node):
+
+    def __init__(self):
+        super().__init__("rpi_video_subsciber")
+        self.sub_ = self.create_subscription(Image, '/rpi_video_feed', self.camCallback, 10)
+        self.bridge=CvBridge()
+
+    def camCallback(self, msg):
+        frame=self.bridge.imgmsg_to_cv2(msg, "bgr8")
+        cv2.imshow('Frame',frame)
+        cv2.waitkey(1)
+
+
+        
+
+
+def main():
+    rclpy.init()
+
+    camera_sub =Camera_sub()
+    
+    rclpy.spin(camera_sub)
+    
+    camera_sub.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
--- sample_pkg/setup.py
+++ sample_pkg/setup.py
@@ -19,7 +19,7 @@ setup(
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
-        'console_scripts': ["cam_node=sample_pkg.video_publisher:main","pub_node=sample_pkg.simple_pub:main","sub_node=sample_pkg.simple_sub:main",
+        'console_scripts': ["cam_node=sample_pkg.video_publisher:main","cam_view_node=sample_pkg.video_subscriber:main","pub_node=sample_pkg.simple_pub:main","sub_node=sample_pkg.simple_sub:main",
         ],
     },
 )
