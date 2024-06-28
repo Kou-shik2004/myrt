edd9003ba1707688f78b3365cb3128d1e23b1859
Kou-shik2004 <koushik20040804@gmail.com>
2024-06-28 21:21:39 +0530

    added test sub node

--- sample_pkg/sample_pkg/video_subscriber.py
+++ sample_pkg/sample_pkg/video_subscriber.py
@@ -17,21 +17,44 @@ class CameraSubscriber(Node):
         frame = self.bridge.compressed_imgmsg_to_cv2(msg.image, "bgr8")
         display_img = frame.copy()
         contours = msg.cnt
-        self.get_logger().info(f'Number of contours received: {len(contours)}')
+
+        # Draw a test rectangle and text
+        cv2.rectangle(display_img, (50, 50), (100, 100), (0, 255, 0), 2)
+        cv2.putText(display_img, "Test", (50, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
         for contour in contours:
-            
             cnt = np.array([(point.x, point.y) for point in contour.points], dtype=np.int32)
             cnt = cnt.reshape((-1, 1, 2))
 
-            x,y,w,h = cv2.boundingRect(cnt)
-            self.get_logger().info(f'Bounding box: x={x}, y={y}, w={w}, h={h}, area={w*h}')
-            if w*h > 1000:
-                cv2.rectangle(display_img,(x,y),(x+w,y+h),(255,0,0),2)
+            x, y, w, h = cv2.boundingRect(cnt)
+            area = w * h
+            self.get_logger().info(f'Bounding box: x={x}, y={y}, w={w}, h={h}, area={area}')
+            
+            if area > 1000:  # Adjust this threshold as needed
+                cv2.rectangle(display_img, (x,y), (x+w,y+h), (255,0,0), 2)
+                cv2.putText(display_img, f'Area: {area}', (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255,0,0), 2)
 
-        cv2.imshow("bound",display_img)
-
+        cv2.imshow("Detected Objects", display_img)
         cv2.waitKey(1)
+    # def cam_callback(self, msg):
+    #     frame = self.bridge.compressed_imgmsg_to_cv2(msg.image, "bgr8")
+    #     display_img = frame.copy()
+    #     contours = msg.cnt
+    #     self.get_logger().info(f'Number of contours received: {len(contours)}')
+
+    #     for contour in contours:
+            
+    #         cnt = np.array([(point.x, point.y) for point in contour.points], dtype=np.int32)
+    #         cnt = cnt.reshape((-1, 1, 2))
+
+    #         x,y,w,h = cv2.boundingRect(cnt)
+    #         self.get_logger().info(f'Bounding box: x={x}, y={y}, w={w}, h={h}, area={w*h}')
+    #         if w*h > 1000:
+    #             cv2.rectangle(display_img,(x,y),(x+w,y+h),(255,0,0),2)
+
+    #     cv2.imshow("bound",display_img)
+
+    #     cv2.waitKey(1)
 
 def main():
     rclpy.init()
