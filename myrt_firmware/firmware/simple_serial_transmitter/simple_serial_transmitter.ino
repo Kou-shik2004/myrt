f2c4b94963bc34ad7a6a57e070086ff0186564b5
Kou-shik2004 <koushik20040804@gmail.com>
2024-07-04 03:32:37 +0530

    added receiver node for serial comms

--- myrt_firmware/firmware/simple_serial_transmitter/simple_serial_transmitter.ino
+++ myrt_firmware/firmware/simple_serial_transmitter/simple_serial_transmitter.ino
@@ -1,20 +1,12 @@
-#define LED_PIN 13
+int x = 0;
+
 void setup() {
-  pinMode(LED_PIN,OUTPUT);
-  digitalWrite(LED_PIN,LOW);
   Serial.begin(115200);
   Serial.setTimeout(1);
 }
 
 void loop() {
-  if(Serial.available()){
-    int x=Serial.readString().toInt();
-    if(x==1){
-      digitalWrite(LED_PIN,HIGH);
-    }
-    else{
-      digitalWrite(LED_PIN,LOW);
-    }
-  }
+  Serial.println(x);
+  x++;
   delay(0.1);
 }
